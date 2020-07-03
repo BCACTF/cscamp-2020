0d8e601e14112aec23b5e6d3e055630f1f1235df
Edward Feng <feng.dingding@gmail.com>
2020-07-03 13:30:06 -0400

    Change binex-is-screaming to use int

--- controlled-screaming/challenge/src.c
+++ controlled-screaming/challenge/src.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    long check = 0;
+    int check = 0;
     char input[64];
 
     setbuf(stdout, NULL);
@@ -19,6 +19,6 @@ int main(void)
         puts("Haha, literally unreachable. Fools.");
         system("/bin/sh");
     } else {
-        printf("See? The check is only 0x%08lx. You'll never get in!\n", check);
+        printf("See? The check is only 0x%08x. You'll never get in!\n", check);
     }
 }
