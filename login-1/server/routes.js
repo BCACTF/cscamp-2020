1caa6b057933b399f973b80d6402af8003b5f889
Anthony Li <li.anthony917@gmail.com>
2020-07-03 11:56:30 -0400

    Add Dockerfile and Vercel support
    > Various other stuff - @edwfeng

--- login-1/server/routes.js
+++ login-1/server/routes.js
@@ -1,5 +1,5 @@
 const { join } = require("path");
-const { readFile } = require("fs/promises");
+const { promises: { readFile } } = require("fs");
 
 const desiredPassword = process.env.LOGIN1_PASSWORD || "someone-misconfigured-this-problem";
 
@@ -8,31 +8,41 @@ module.exports = {
         res.sendFile(join(__dirname, "index.html"));
     },
     js: async (_, res) => {
-        const content = await readFile(join(__dirname, "securelogin.js"), "utf8");
-        res.type("text/javascript");
-        res.send(content.replace("{INSERT PASSWORD HERE}", desiredPassword
-            .replace(/\\/g, "\\\\")
-            .replace(/"/g, "\\\"")
-            .replace(/\n/g, "\\n")));
+        try {
+            const content = await readFile(join(__dirname, "securelogin.js"), "utf8");
+            res.setHeader("content-type", "text/javascript; charset=utf-8");
+            res.send(content.replace("{INSERT PASSWORD HERE}", desiredPassword
+                .replace(/\\/g, "\\\\")
+                .replace(/"/g, "\\\"")
+                .replace(/\n/g, "\\n")));
+        } catch (e) {
+            console.log(e);
+            res.status(500);
+            res.send("Internal Server Error; Contact CS Camp staff");
+        }
     },
     flag: async ({ body, method }, res) => {
         if (method !== "POST") {
             res.status(405);
             res.text("Nice try.");
+            console.log("/flag: Wrong method");
             return;
         }
 
         if (!body) {
             res.status(400);
             res.text("Malformed JSON.");
+            console.log("/flag: Bad request");
             return;
         }
 
         const { username, password } = body;
 
         if (username === "admin" && password === desiredPassword) {
+            console.log("/flag: Success");
             res.json({flag: process.env.LOGIN1_FLAG || "This problem is miconfigured. Please contact CS Camp staff."})
         } else {
+            console.log("/flag: Incorrect credentials");
             res.status(401);
             res.text("Nice try.");
         }
