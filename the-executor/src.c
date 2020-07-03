f8400124c8a442726d259ca3c6f4a272f2cf3a45
Edward Feng <feng.dingding@gmail.com>
2020-07-03 14:18:20 -0400

    Switch to int array in the-executor to prevent running `strings`

--- the-executor/src.c
+++ the-executor/src.c
@@ -2,7 +2,16 @@
 
 int main()
 {
+    static const int flag[] = {99, 97, 109, 112, 123, 69, 120, 51, 99, 117, 55, 105, 78, 103, 95, 98, 49, 110, 52, 114, 105, 51, 53, 95, 108, 49, 107, 51, 95, 52, 95, 112, 82, 48, 125};
+    int i;
+
     puts("Welcome to the CS Camp CTF!");
     puts("Let's start off binex easy: simply executing a program.");
-    puts("Here's your flag: camp{Ex3cu7iNg_b1n4ri35_l1k3_4_pR0}");
+    printf("Here's your flag: ");
+
+    for (i = 0; i < 35; i++) {
+        printf("%c", flag[i]);
+    }
+
+    printf("\n");
 }
