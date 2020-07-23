c4bda3bee8dd7f513c40d6c67664a5519f0424a7
Anthony Li <li.anthony917@gmail.com>
2020-07-22 22:46:06 -0400

    Add flagjs-1 problem

---
+++ flagjs-1/flag.js
@@ -0,0 +1,18 @@
+window.FlagJS = {
+    getFlag(flag) {
+        const flags = {
+            ctf: "camp{g0tta_l0v3_th4t_js_c0n50l3_v678en}",
+            inHole: "⛳",
+            chequered: "🏁",
+            crossed: "🎌",
+            triangular: "🚩",
+            black: "🏴",
+            white: "🏳️",
+            rainbow: "🏳️‍🌈",
+            pirate: "🏴‍☠️",
+            transgender: "🏳️‍⚧️"
+        };
+
+        return flags[flag];
+    }
+};
